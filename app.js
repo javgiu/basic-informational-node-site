6476997987c7c4c8a38b8a8568070de9e92eba23
javgiu <javypicallo@gmail.com>
2026-05-21 21:54:13 -0400

    mend

--- app.js
+++ app.js
@@ -47,15 +47,19 @@ const __dirname = import.meta.dirname;
 app.get("/", (req, res) => {
     res.sendFile("index.html", { root: __dirname });
 });
+
 app.get("/about", (req, res) => {
     res.sendFile("about.html", { root: __dirname });
 });
+
 app.get("/contact", (req, res) => {
     res.sendFile("contact-me.html", { root: __dirname });
 });
+
 app.use((req, res) => {
     res.status(404).sendFile("404.html", { root: __dirname });
 });
+
 app.listen(PORT, hostname, (error) => {
     if (error) {
         throw error;
